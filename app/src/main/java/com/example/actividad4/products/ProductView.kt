3449716c8952d1aaf214564dc22a888221fe115d
oNovella <omar.novella@iest.edu.mx>
2026-04-11 01:46:46 -0600

    update: Actualizacion final con solucion de errores de gradle

--- app/src/main/java/com/example/actividad4/products/ProductView.kt
+++ app/src/main/java/com/example/actividad4/products/ProductView.kt
@@ -1,13 +1,25 @@
 package com.example.actividad4.products
 
 import androidx.annotation.DrawableRes
+<<<<<<< HEAD
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+=======
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+<<<<<<< HEAD
+=======
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -17,22 +29,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+<<<<<<< HEAD
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+=======
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.actividad4.R
+<<<<<<< HEAD
+=======
+import kotlin.math.min
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
 
 @Composable
 fun ProductView(product: ProductModel){
     Card(
+<<<<<<< HEAD
         colors = CardDefaults.cardColors(Color.White),
         modifier = Modifier
             .fillMaxWidth()
     ) {
 
+=======
+        shape = RectangleShape,
+        border = BorderStroke(1.dp, Color.LightGray),
+        colors = CardDefaults.cardColors(Color.White),
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min= 140.dp)
+    ) {
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
         Column(
             Modifier
                 .fillMaxWidth()
@@ -62,18 +96,36 @@ fun RowGeneration(
     discountPercent: Float,
     @DrawableRes imageRoute: Int
 ) {
+<<<<<<< HEAD
     Row {
+=======
+    Row(modifier = Modifier.fillMaxWidth()) {
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
         Image(
             painter = painterResource(imageRoute),
             contentDescription = name,
             modifier = Modifier
                 .size(120.dp)
                 .align(Alignment.CenterVertically)
+<<<<<<< HEAD
         )
 
         Column(modifier = Modifier.padding(4.dp)) {
             Spacer(Modifier.size(3.dp))
             Text(name, fontSize = 20.sp)
+=======
+                .padding(8.dp)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                //.fillMaxWidth()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Spacer(Modifier.size(3.dp))
+            Text(name, fontSize = 20.sp, maxLines = 2, overflow = TextOverflow.Ellipsis)
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
             Text("Por ${sellerName}", fontSize = 10.sp)
 
             Spacer(Modifier.size(10.dp))
--- app/src/main/java/com/example/actividad4/products/ProductsRepository.kt
+++ app/src/main/java/com/example/actividad4/products/ProductsRepository.kt
@@ -3,29 +3,52 @@ package com.example.actividad4.products
 import com.example.actividad4.R
 
 class ProductsRepository {
+<<<<<<< HEAD
     private val productsPerCategory = mapOf(
         "electronica" to listOf(
             ProductModel(
                 productName = "iPhone 17 Pro Max",
                 productCost = 40_550f,
+=======
+
+    private val productsPerCategory: Map<String, List<ProductModel>> = mapOf(
+
+        "electronica" to listOf(
+            ProductModel(
+                productName = "iPhone 17 Pro Max",
+                productCost = 40550f,
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
                 seller = "Apple",
                 shippingCost = 0f,
                 discountPercent = 15f,
                 starsRate = 4.3f,
                 productImage = R.drawable.iphone17pro
+<<<<<<< HEAD
 
             ),
             ProductModel(
                 productName = "MacBook Air 13 pulgadas, M3 10-Core",
                 productCost = 20_810f,
+=======
+            ),
+            ProductModel(
+                productName = "MacBook Air 13 pulgadas, M3 10-Core",
+                productCost = 20810f,
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
                 seller = "Apple",
                 shippingCost = 0f,
                 discountPercent = 10f,
                 starsRate = 3.2f,
                 productImage = R.drawable.macbook
+<<<<<<< HEAD
 
             ),
         ),
+=======
+            )
+        ),
+
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
         "mobiliario" to listOf(
             ProductModel(
                 productName = "Juego de Oficina color obsidiana",
@@ -49,16 +72,87 @@ class ProductsRepository {
                 productName = "Silla acolchonada gris de terciopelo",
                 productCost = 7700f,
                 seller = "Liverpool Oficial",
+<<<<<<< HEAD
                 productImage = R.drawable.images,
+=======
+                productImage = R.drawable.comfy_chair,
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
                 discountPercent = 0f,
                 starsRate = 4f,
                 shippingCost = 450f
             )
+<<<<<<< HEAD
         )
 
     )
 
     fun getProductsPerCategory(category: String): List<ProductModel>{
+=======
+        ),
+
+        "automotriz" to listOf(
+            ProductModel(
+                productName = "Aceite para motor Castrol Edge Syntec",
+                productCost = 550f,
+                seller = "AutoZone",
+                shippingCost = 150f,
+                discountPercent = 5f,
+                starsRate = 4.5f,
+                productImage = R.drawable.c5w50_syn_1_x1000
+            ),
+            ProductModel(
+                productName = "Jeep Wagonner 3.0 Premium 4x4 2025",
+                productCost = 1_988_900f,
+                seller = "Jeep",
+                shippingCost = 0f,
+                discountPercent = 6f,
+                starsRate = 4.5f,
+                productImage = R.drawable.d_q_np_610111_mlm106726395460_022026_b
+            ),
+            ProductModel(
+                productName = "Nissan Frontier 2.5 Doble Cabina 4x2 2025",
+                productCost = 605_000f,
+                seller = "Grupo WITT",
+                shippingCost = 0f,
+                discountPercent = 0f,
+                starsRate = 0f,
+                productImage = R.drawable.d_q_np_845304_mlm109371556688_042026_b
+            ),
+            ProductModel(
+                productName = "Kia Sorento 2.5 SXL 2026",
+                productCost = 988_900f,
+                seller = "Premier Automotriz",
+                shippingCost = 0f,
+                discountPercent = 0f,
+                starsRate = 4.5f,
+                productImage = R.drawable.d_q_np_998938_mlm105025628855_012026_b
+            ),
+            ProductModel(
+                productName = "Llanta 265/70r17 Scorpion All Terrain",
+                productCost = 3290f,
+                seller = "Car World Center",
+                shippingCost = 0f,
+                discountPercent = 6f,
+                starsRate = 4.7f,
+                productImage = R.drawable.d_q_np_897643_mla99338538956_112025_b
+            )
+        ),
+
+        "salud" to listOf(
+            ProductModel(
+                productName = "Proteína Whey",
+                productCost = 1200f,
+                seller = "GNC",
+                shippingCost = 0f,
+                discountPercent = 10f,
+                starsRate = 4.7f,
+                productImage = R.drawable.macbook // cambia esto
+            )
+        )
+    )
+
+    fun getProductsPerCategory(category: String): List<ProductModel> {
+>>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
         return productsPerCategory[category] ?: emptyList()
     }
 }
