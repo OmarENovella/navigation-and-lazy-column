b4e3bafaf16a799caf988f291d33e2a09a481cf9
OmarENovella <omar.novella@iest.edu.mx>
2026-04-20 18:00:15 -0600

    Reparacion de errores

--- app/src/main/java/com/example/actividad4/products/ProductView.kt
+++ app/src/main/java/com/example/actividad4/products/ProductView.kt
@@ -1,25 +1,16 @@
 package com.example.actividad4.products
 
 import androidx.annotation.DrawableRes
-<<<<<<< HEAD
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-=======
+
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
->>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-<<<<<<< HEAD
-=======
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
->>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -29,33 +20,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-<<<<<<< HEAD
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-=======
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
->>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.actividad4.R
-<<<<<<< HEAD
-=======
-import kotlin.math.min
 
 @Composable
 fun ProductView(product: ProductModel){
     Card(
-        colors = CardDefaults.cardColors(Color.White),
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-
         shape = RectangleShape,
         border = BorderStroke(1.dp, Color.LightGray),
         colors = CardDefaults.cardColors(Color.White),
@@ -63,7 +40,7 @@ fun ProductView(product: ProductModel){
             .fillMaxWidth()
             .heightIn(min= 140.dp)
     ) {
->>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
+
         Column(
             Modifier
                 .fillMaxWidth()
@@ -93,26 +70,14 @@ fun RowGeneration(
     discountPercent: Float,
     @DrawableRes imageRoute: Int
 ) {
-<<<<<<< HEAD
-    Row {
-=======
     Row(modifier = Modifier.fillMaxWidth()) {
->>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
         Image(
             painter = painterResource(imageRoute),
             contentDescription = name,
             modifier = Modifier
                 .size(120.dp)
-                .align(Alignment.CenterVertically)
-<<<<<<< HEAD
-        )
+                .align(Alignment.CenterVertically))
 
-        Column(modifier = Modifier.padding(4.dp)) {
-            Spacer(Modifier.size(3.dp))
-            Text(name, fontSize = 20.sp)
-=======
-                .padding(8.dp)
-        )
         Column(
             modifier = Modifier
                 .weight(1f)
@@ -122,7 +87,6 @@ fun RowGeneration(
         ) {
             Spacer(Modifier.size(3.dp))
             Text(name, fontSize = 20.sp, maxLines = 2, overflow = TextOverflow.Ellipsis)
->>>>>>> 36d9921 (update: Primera version y respaldo de trabajo en actividad 4 (incompleta))
             Text("Por ${sellerName}", fontSize = 10.sp)
 
             Spacer(Modifier.size(10.dp))
